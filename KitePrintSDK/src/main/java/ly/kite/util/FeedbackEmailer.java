eaafa9d9a07845f0e2c0069f530922bbff82ed59
JLKite <jluser@jlkira>
2015-10-30 10:29:44 +0000

    Moved feedback emailer into SDK

---
+++ KitePrintSDK/src/main/java/ly/kite/util/FeedbackEmailer.java
@@ -0,0 +1,112 @@
+/*****************************************************
+ *
+ * FeedbackEmailer.java
+ *
+ *
+ * Modified MIT License
+ *
+ * Copyright (c) 2010-2015 Kite Tech Ltd. https://www.kite.ly
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The software MAY ONLY be used with the Kite Tech Ltd platform and MAY NOT be modified
+ * to be used with any competitor platforms. This means the software MAY NOT be modified
+ * to place orders with any competitors to Kite Tech Ltd, all orders MUST go through the
+ * Kite Tech Ltd platform servers.
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ *****************************************************/
+
+///// Package Declaration /////
+
+package ly.kite.util;
+
+
+///// Import(s) /////
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import ly.kite.R;
+
+
+///// Class Declaration /////
+
+/*****************************************************
+ *
+ * Sends a feedback / support email.
+ *
+ *****************************************************/
+public class FeedbackEmailer
+  {
+  ////////// Static Constant(s) //////////
+
+  @SuppressWarnings( "unused" )
+  private static final String  LOG_TAG = "FeedbackEmailer";
+
+
+  ////////// Static Variable(s) //////////
+
+
+  ////////// Member Variable(s) //////////
+
+
+  ////////// Static Initialiser(s) //////////
+
+
+  ////////// Static Method(s) //////////
+
+  /*****************************************************
+   *
+   * Send an email.
+   *
+   *****************************************************/
+  static public void sendEmail( Context context )
+    {
+    Intent emailIntent = new Intent( Intent.ACTION_SENDTO, Uri.fromParts( "mailto", context.getString( R.string.feedback_email_recipient ), null ) );
+
+    emailIntent.putExtra( Intent.EXTRA_SUBJECT, context.getString( R.string.feedback_email_subject ) );
+    emailIntent.putExtra( Intent.EXTRA_TEXT, context.getString( R.string.feedback_email_text ) );
+
+    context.startActivity( Intent.createChooser( emailIntent, context.getString( R.string.send_email_chooser_title ) ) );
+    }
+
+
+  ////////// Constructor(s) //////////
+
+
+  ////////// Method(s) //////////
+
+  /*****************************************************
+   *
+   * ...
+   *
+   *****************************************************/
+
+
+  ////////// Inner Class(es) //////////
+
+  /*****************************************************
+   *
+   * ...
+   *
+   *****************************************************/
+
+  }
+
