31e022356790df5f33f31c2693dcc41819a4b3cf
JLKite <jluser@jlkira>
2016-07-28 18:11:37 +0100

    Updated Instagram photo picker version (with translations); added better error reporting for failed orders; fixed basket formatting issue;

--- KitePrintSDK/src/main/java/ly/kite/ordering/Order.java
+++ KitePrintSDK/src/main/java/ly/kite/ordering/Order.java
@@ -645,56 +645,86 @@ public class Order implements Parcelable /* , Serializable */
       }
 
 
-      public void submitForPrinting(Context context, ISubmissionProgressListener listener) {
-        if (userSubmittedForPrinting) throw new AssertionError("A PrintOrder can only be submitted once unless you cancel the previous submission");
-        //if (proofOfPayment == null) throw new AssertionError("You must provide a proofOfPayment before you can submit a print order");
-        if (printOrderReq != null) throw new AssertionError("A PrintOrder request should not already be in progress");
+    public void submitForPrinting( Context context, ISubmissionProgressListener listener )
+      {
+      this.submissionListener = listener;
 
-        lastPrintSubmissionDate = new Date();
-        userSubmittedForPrinting = true;
+      if ( userSubmittedForPrinting )
+        {
+        notifyIllegalStateError( "An order has already been submitted for printing. An order submission must be cancelled before it can be submitted again." );
 
-        this.submissionListener = listener;
-        if (assetUploadComplete) {
-            submitForPrinting( context );
-        } else if (!isAssetUploadInProgress()) {
-            startAssetUpload(context);
+        return;
         }
-    }
 
-    private void submitForPrinting( Context context ) {
-        if (!userSubmittedForPrinting) throw new IllegalStateException("oops");
-        if (!assetUploadComplete || isAssetUploadInProgress()) throw new IllegalStateException("Oops asset upload should be complete by now");
 
-        // Step 2: Submit print order to the server. Print Job JSON can now reference real asset ids.
-        printOrderReq = new SubmitOrderRequest(this);
-        printOrderReq.submitForPrinting( context, new SubmitOrderRequest.IProgressListener()
+      if ( printOrderReq != null )
+        {
+        notifyIllegalStateError( "A print order request is already in progress." );
+
+        return;
+        }
+
+      lastPrintSubmissionDate = new Date();
+      userSubmittedForPrinting = true;
+
+      if ( assetUploadComplete )
+        {
+        submitForPrinting( context );
+        }
+      else if ( !isAssetUploadInProgress() )
+        {
+        startAssetUpload( context );
+        }
+      }
+
+
+    private void submitForPrinting( Context context )
+      {
+      if ( ! userSubmittedForPrinting )
+        {
+        notifyIllegalStateError( "The order cannot be submitted for printing if it has not been marked as submitted" );
+
+        return;
+        }
+
+      if ( ! assetUploadComplete || isAssetUploadInProgress() )
+        {
+        notifyIllegalStateError( "The order should not be submitted for priting until the asset upload has completed." );
+
+        return;
+        }
+
+      // Step 2: Submit print order to the server. Print Job JSON can now reference real asset ids.
+      printOrderReq = new SubmitOrderRequest( this );
+      printOrderReq.submitForPrinting( context, new SubmitOrderRequest.IProgressListener()
         {
         @Override
         public void onSubmissionComplete( SubmitOrderRequest req, String orderId )
-            {
-            // The initial submission was successful, but note that this doesn't mean the order will
-            // pass validation. It may fail subsequently when we are polling the order status. So remember
-            // to clear the receipt / set the error if we find a problem later.
+          {
+          // The initial submission was successful, but note that this doesn't mean the order will
+          // pass validation. It may fail subsequently when we are polling the order status. So remember
+          // to clear the receipt / set the error if we find a problem later.
 
-            setReceipt( orderId );
+          setReceipt( orderId );
 
-            printOrderReq = null;
+          printOrderReq = null;
 
-            submissionListener.onSubmissionComplete( Order.this, orderId );
-            }
+          submissionListener.onSubmissionComplete( Order.this, orderId );
+          }
 
         @Override
         public void onError( SubmitOrderRequest req, Exception error )
-            {
-            userSubmittedForPrinting = false;
-            lastPrintSubmissionError = error;
+          {
+          userSubmittedForPrinting = false;
+          lastPrintSubmissionError = error;
 
-            printOrderReq = null;
+          printOrderReq = null;
 
-            submissionListener.onError( Order.this, error );
-            }
+          notifyError( error );
+          }
         } );
-    }
+      }
+
 
     public void cancelSubmissionOrPreemptedAssetUpload() {
         if (assetUploadReq != null) {
@@ -866,6 +896,31 @@ public class Order implements Parcelable /* , Serializable */
 
     /*****************************************************
      *
+     * Notifies any listener of an error.
+     *
+     *****************************************************/
+    private void notifyError( Exception exception )
+      {
+      if ( submissionListener != null )
+        {
+        submissionListener.onError( this, exception );
+        }
+      }
+
+
+    /*****************************************************
+     *
+     * Notifies any listener of an illegal state error.
+     *
+     *****************************************************/
+    private void notifyIllegalStateError( String message )
+      {
+      notifyError( new IllegalStateException( message ) );
+      }
+
+
+    /*****************************************************
+     *
      * Resets the order state with an error. This is likely
      * to have been returned from polling the order status.
      *
@@ -912,11 +967,15 @@ public class Order implements Parcelable /* , Serializable */
 
         // Check that all the assets scheduled to be uploaded, were
 
-        for ( UploadableImage uploadedAsset : uploadedImages )
+        for ( UploadableImage uploadedImage : uploadedImages )
           {
-          if ( ! mImagesToUpload.contains( uploadedAsset ) )
+          if ( ! mImagesToUpload.contains( uploadedImage ) )
             {
-            throw new AssertionError( "Oops - found an asset not in the upload list" );
+            Log.e( LOG_TAG, "Found image not in upload list: " + uploadedImage );
+
+            notifyIllegalStateError( "An image has been uploaded that shouldn't have been" );
+
+            return;
             }
           }
 
@@ -948,7 +1007,11 @@ public class Order implements Parcelable /* , Serializable */
             {
             if ( uploadableImage != null && ! uploadableImage.hasBeenUploaded() )
               {
-              throw new AssertionError( "Oops ... all assets should have been uploaded" );
+              Log.e( LOG_TAG, "An image that should have been uploaded, hasn't been." );
+
+              notifyIllegalStateError( "An image that should have been uploaded, hasn't been." );
+
+              return;
               }
             }
           }
