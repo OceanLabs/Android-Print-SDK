8186f95df83acba93af221da5d15a16f7cdc53ff
Sarah Sharp <animefly@gmail.com>
2018-04-24 10:48:15 +0100

    Add Dutch

--- KitePrintSDK/src/main/java/ly/kite/KiteSDK.java
+++ KitePrintSDK/src/main/java/ly/kite/KiteSDK.java
@@ -110,7 +110,7 @@ public class KiteSDK
   static public  final boolean DISPLAY_PRODUCTS                                    = false;
 
 
-  static public  final String SDK_VERSION                                          = "5.8.4";
+  static public  final String SDK_VERSION                                          = "5.8.5";
 
   static public  final String IMAGE_CATEGORY_APP                                   = "app";
   static public  final String IMAGE_CATEGORY_PRODUCT_ITEM                          = "product_item";
