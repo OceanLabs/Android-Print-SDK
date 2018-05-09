a736c2da0d7421a23cfa03071faefa8934d8bc61
andreimacaveiu <macaveiuandrei@gmail.com>
2018-05-09 17:29:32 +0100

    AND-88: Cancel button doesn't work for multi-image products fix

--- KitePrintSDK/src/main/java/ly/kite/journey/creation/reviewandedit/EditImageFragment.java
+++ KitePrintSDK/src/main/java/ly/kite/journey/creation/reviewandedit/EditImageFragment.java
@@ -142,6 +142,8 @@ public class EditImageFragment extends AEditImageFragment
       {
       setBackwardsTextViewVisibility( View.VISIBLE );
       setBackwardsTextViewText( backwardsText );
+      setBackwardsTextViewEnabled(true);
+      setBackwardsTextViewOnClickListener(this);
       }
     else
       {
